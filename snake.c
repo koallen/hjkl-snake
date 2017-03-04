900a72e12a00f51d5bf8818b91bfdac1b229f073
koallen <weifengzi2009@gmail.com>
2017-03-04 10:19:22 +0800

    code refactor

--- snake.c
+++ snake.c
@@ -6,8 +6,8 @@
 #define DELAY 130000
 
 typedef struct {
-	int x_direction;
-	int y_direction;
+	int x;
+	int y;
 } Direction;
 
 typedef struct {
@@ -15,8 +15,26 @@ typedef struct {
 	int y;
 } Point;
 
-void MoveSnake(Point *, Direction, int *, Point, bool *, int, int, bool *);
-void GenerateFood(Point *, int, int, int, Point *, bool *);
+typedef struct {
+	Point location;
+	bool available;
+} Food;
+
+typedef struct {
+	Point *body;
+	int length;
+	Direction direction;
+	bool dead;
+} Snake;
+
+typedef struct {
+	int x;
+	int y;
+} WindowSize;
+
+void MoveSnake(WindowSize, Snake *, Food *);
+void GenerateFood(WindowSize, Snake, Food *);
+void UpdateDirection(char, Snake *);
 
 int main()
 {
@@ -27,33 +45,29 @@ int main()
 	noecho();
 	curs_set(FALSE);
 	nodelay(stdscr, TRUE); // don't block on getch() call
-	int max_x, max_y;
-	getmaxyx(stdscr, max_y, max_x); // get window size
+	WindowSize window_size = {0, 0};
+	getmaxyx(stdscr, window_size.y, window_size.x); // get window size
 
 	/*
 	 * initialize game
 	 */
 	srand(time(NULL));
 
-	Point *snake = (Point *)malloc(sizeof(Point));
-	int snake_length = 1;
-	bool dead = false;
-	snake[0].x = 0;
-	snake[0].y = 0;
+	Snake snake = {(Point *)malloc(sizeof(Point)), 1, {0, 0}, false};
+	snake.body[0].x = 0;
+	snake.body[0].y = 0;
 
-	Direction direction = {0, 0};
-	Point food;
-	bool food_available = false;
-
+	Food food = {{0, 0}, false};
 
 	/*
 	 * main loop
 	 */
-	mvprintw(max_y, 0, "Quit the game with 'q'");
-	max_y--; // last line reserved for the above instruction
-	mvprintw(snake[0].y, snake[0].x, "o");
-	GenerateFood(snake, snake_length, max_x, max_y, &food, &food_available);
-	mvprintw(food.y, food.x, "=");
+	mvprintw(window_size.y, 0, "Quit the game with 'q'");
+	window_size.y--; // last line reserved for the above instruction
+	mvprintw(snake.body[0].y, snake.body[0].x, "o");
+	GenerateFood(window_size, snake, &food);
+	mvprintw(food.location.y, food.location.x, "=");
+
 	refresh();
 
 	while (1)
@@ -61,37 +75,24 @@ int main()
 		char key_stroke = getch();
 		switch (key_stroke)
 		{
-			case 'h':
-				direction.x_direction = -1;
-				direction.y_direction = 0;
-				break;
-			case 'j':
-				direction.x_direction = 0;
-				direction.y_direction = 1;
-				break;
-			case 'k':
-				direction.x_direction = 0;
-				direction.y_direction = -1;
-				break;
-			case 'l':
-				direction.x_direction = 1;
-				direction.y_direction = 0;
-				break;
 			case 'q':
 				goto END;
-				break;
+			default:
+				UpdateDirection(key_stroke, &snake);
 		}
 
 		clear();
-		mvprintw(max_y, 0, "Quit the game with 'q'");
-		MoveSnake(snake, direction, &snake_length, food, &food_available, max_x, max_y, &dead);
-		// check death
-		if (dead)
-			goto END;
-		// check food
-		if (!food_available)
-			GenerateFood(snake, snake_length, max_x, max_y, &food, &food_available);
-		mvprintw(food.y, food.x, "=");
+
+		mvprintw(window_size.y, 0, "Quit the game with 'q'");
+		MoveSnake(window_size, &snake, &food);
+
+		if (snake.dead)
+			break;
+
+		if (!food.available)
+			GenerateFood(window_size, snake, &food);
+		mvprintw(food.location.y, food.location.x, "=");
+
 		refresh();
 		usleep(DELAY);
 	}
@@ -105,62 +106,84 @@ END:
 	return EXIT_SUCCESS;
 }
 
-void MoveSnake(Point *snake, Direction direction, int *snake_length, Point food, bool *food_available, int max_x, int max_y, bool *dead)
+void MoveSnake(WindowSize window_size, Snake *snake, Food *food)
 {
-	int old_head_x = snake[0].x, old_head_y = snake[0].y;
+	int old_head_x = snake->body[0].x, old_head_y = snake->body[0].y;
 	int new_head_x, new_head_y;
-	new_head_x = old_head_x + direction.x_direction;
-	new_head_y = old_head_y + direction.y_direction;
+	new_head_x = old_head_x + snake->direction.x;
+	new_head_y = old_head_y + snake->direction.y;
 
 	// detect collision
-	if (new_head_x < 0 || new_head_x > max_x || new_head_y < 0 || new_head_y > max_y)
+	if (new_head_x < 0 || new_head_x > window_size.x || new_head_y < 0 || new_head_y > window_size.y)
 	{
-		*dead = true;
+		snake->dead = true;
 		return;
 	}
-	for (int i = 1; i < *snake_length; ++i)
-		if (new_head_x == snake[i].x && new_head_y == snake[i].y)
+	for (int i = 1; i < snake->length; ++i)
+		if (new_head_x == snake->body[i].x && new_head_y == snake->body[i].y)
 		{
-			*dead = true;
+			snake->dead = true;
 			return;
 		}
 
 	// detect whether food is eaten by next move
-	if (new_head_x == food.x && new_head_y == food.y)
+	if (new_head_x == food->location.x && new_head_y == food->location.y)
 	{
-		*food_available = false;
-		(*snake_length)++;
-		snake = realloc(snake, (*snake_length) * sizeof(Point)); // extend the snake by 1
+		food->available = false;
+		snake->length++;
+		snake->body = realloc(snake->body, snake->length * sizeof(Point)); // extend the snake by 1
 	}
 
 	// move snake
-	for (int i = *snake_length - 1; i > 0; --i)
+	for (int i = snake->length - 1; i > 0; --i)
 	{
-		snake[i].x = snake[i-1].x;
-		snake[i].y = snake[i-1].y;
+		snake->body[i].x = snake->body[i-1].x;
+		snake->body[i].y = snake->body[i-1].y;
 	}
-	snake[0].x = new_head_x;
-	snake[0].y = new_head_y;
+	snake->body[0].x = new_head_x;
+	snake->body[0].y = new_head_y;
 
-	for (int i = 0; i < *snake_length; ++i)
-		mvprintw(snake[i].y, snake[i].x, "o");
-
+	for (int i = 0; i < snake->length; ++i)
+		mvprintw(snake->body[i].y, snake->body[i].x, "o");
 }
 
-void GenerateFood(Point *snake, int snake_length, int max_x, int max_y, Point *food, bool *food_available)
+void GenerateFood(WindowSize window_size, Snake snake, Food *food)
 {
 	bool food_on_snake = true;
 
 	// generate a food which is not on the body of snake
 	while (food_on_snake)
 	{
-		food->x = rand() % max_x;
-		food->y = rand() % max_y;
-		for (int i = 0; i < snake_length; ++i)
-			if (food->x == snake[i].x && food->y == snake[i].y)
+		food->location.x = rand() % window_size.x;
+		food->location.y = rand() % window_size.y;
+		for (int i = 0; i < snake.length; ++i)
+			if (food->location.x == snake.body[i].x && food->location.y == snake.body[i].y)
 				food_on_snake = true;
 		food_on_snake = false;
 	}
 
-	*food_available = true;
+	food->available = true;
+}
+
+void UpdateDirection(char key_stroke, Snake *snake)
+{
+	switch(key_stroke)
+	{
+		case 'h':
+			snake->direction.x = -1;
+			snake->direction.y = 0;
+			break;
+		case 'j':
+			snake->direction.x = 0;
+			snake->direction.y = 1;
+			break;
+		case 'k':
+			snake->direction.x = 0;
+			snake->direction.y = -1;
+			break;
+		case 'l':
+			snake->direction.x = 1;
+			snake->direction.y = 0;
+			break;
+	}	
 }
